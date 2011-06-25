bb551a78a68eb38685a5e004b3e921e2b4c5acff
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-06-25 19:07:15 +0200

    Using relative paths.

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -2,11 +2,11 @@ require 'ostruct'
 
 class Configuration < OpenStruct
   def initialize(*envs)
-    shared_config = YAML.load_file(File.join(File.dirname(__FILE__), "../config.yml"))
+    shared_config = YAML.load_file("config.yml")
     
     config = shared_config
     envs.each do |env|
-      env_config_file = File.join(File.dirname(__FILE__), "../envs/#{env}/config.yml")
+      env_config_file = "envs/#{env}/config.yml"
 
       config = File.exists?(env_config_file) ?
                YAML.load_file(env_config_file).merge(config) :
