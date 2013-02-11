c338c9f6b1a32405e0f94b937e8af3667a42a869
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-02-11 14:29:58 +0100

    Hm... fix path.

--- cookbooks/site/ruby-shadow/recipes/default.rb
+++ cookbooks/site/ruby-shadow/recipes/default.rb
@@ -2,7 +2,7 @@ site_ruby = "/usr/local/lib/ruby/site_ruby/1.8"
 
 remote_directory "/usr/local/src/shadow-1.4.1" do
   source 'shadow-1.4.1'
-  not_if { File.exists?(site_ruby, "#{node[:languages][:ruby][:platform]}/shadow.so")) }
+  not_if { File.exists?(File.join(site_ruby, "#{node[:languages][:ruby][:platform]}/shadow.so")) }
 end
 
 bash "install ruby shadow library" do
@@ -13,5 +13,5 @@ bash "install ruby shadow library" do
   ruby extconf.rb
   make install
   EOH
-  not_if { File.exists?(site_ruby, "/#{node[:languages][:ruby][:platform]}/shadow.so")) }
+  not_if { File.exists?(File.join(site_ruby, "/#{node[:languages][:ruby][:platform]}/shadow.so")) }
 end
