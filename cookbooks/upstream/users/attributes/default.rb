8545e011e9ed8e9d09bcb3e079ea96dc7affd323
Henrik Nyh <henrik@nyh.se>
2013-11-27 13:15:33 +0100

    Fix node.set deprecation.
    Just calling `users Mash.new` called `method_missing` which ended up doing the deprecated `node["users"] = Mash.new`.

--- cookbooks/upstream/users/attributes/default.rb
+++ cookbooks/upstream/users/attributes/default.rb
@@ -1,4 +1,6 @@
-users Mash.new unless attribute?("users")
+unless attribute?("users")
+  node.set["users"] = Mash.new
+end
 
 # passwords must be in shadow password format with a salt. To generate: openssl passwd -1
 # users[:jose] = {:password => "shadowpass", :comment => "José Amador", :ssh_key => "..." }
